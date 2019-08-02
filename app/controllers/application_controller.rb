9ad4e140b851c33cbaf2dada31d97c1e4817054d
github username <github email address>
2019-08-02 14:05:34 +0100

    Added homepage picture

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  helper_method :current_user, :authorize_user, current_user_interests
+  helper_method :current_user, :authorize_user
 
   def current_user 
     if session[:user_id]
@@ -12,10 +12,6 @@ class ApplicationController < ActionController::Base
       flash[:notice] = "Sorry, you need to be logged in to access that feature."
       redirect_to login_path
     end 
-  end 
-
-  def current_user_interests
-    current_user.interests
   end 
 
 
